14d944b9b06086f5f7ebc103b69e0e3d21c16189
gilbertopir <gilnial@gmail.com>
2026-05-10 11:18:30 +0100

    Fix missing settings import in views.py

--- site_app/views.py
+++ site_app/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse, HttpResponse
 from django.contrib import messages
+from django.conf import settings
 
 from .models import Alignment, FeatureCapture, PassingPlace
 from .utils import (
